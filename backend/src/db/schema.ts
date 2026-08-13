864652264d63708c7d1f5dc66bf9ad9d14ae9672
Izaya-Letselane <139796837+Izaya-Letselane@users.noreply.github.com>
2026-08-13 16:52:55 +0200

    2nd commit

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,88 @@
+import {pgTable,text,integer,timestamp,uuid,jsonb,boolean} from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+
+export type OrderStatus = "pending"|"paid"|"failed"
+export type UserRole = "customer"| "support"| "admin"
+export type CheckoutSessionLine = {
+    productId:string;
+    quantity:number;
+    unitPriceCents: number
+}
+export const users = pgTable("users",{
+    id: uuid("id").defaultRandom().primaryKey(),
+    clerkUserId: text("clerk_user_id").notNull().unique(),
+    email:  text("email").notNull().default(""),
+    displayName:  text("display_name"),
+    role: text("role").$type<UserRole>().notNull().default("customer"),
+    createdAt: timestamp("created_at", {withTimezone:true}).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", {withTimezone:true}).defaultNow().notNull(),
+})
+
+export const products = pgTable("products",{
+    id: uuid("id").defaultRandom().primaryKey(),
+    slug: text("slug").notNull().unique(),/**Nike=>nike-shoe */
+    name: text("name").notNull(),
+    category: text("category").notNull().default("General"),
+    description: text("description").notNull().default(""),
+    priceCents: text("price_cents").notNull(),
+    currency: text("currency").notNull().default("zar"),
+    imageKitFileId: text("image"),
+    active: boolean("active").notNull().default(true),
+    
+    createdAt: timestamp("created_at", {withTimezone:true}).defaultNow().notNull(),    
+
+})
+export const CheckoutSessions = pgTable("checkout_sessions",{
+   id: uuid("id").defaultRandom().primaryKey(),
+   userId: uuid("user_id")
+   .notNull().references(()=>users.id, {onDelete: 'cascade'}),
+   polarCheckoutId: text("polar_checkout_id").unique(),
+   lines: jsonb("lines").$type<CheckoutSessionLine[]>().notNull(),
+   totalCents: integer("total_cents").notNull(),
+   currency:text("currency").notNull(),
+    createdAt: timestamp("created_at", {withTimezone:true}).defaultNow().notNull(),
+})
+//cascade = delete children the parents are deleted; restrict- dont delete parent when child is deleted
+
+export const orders = pgTable("orders", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    userId: uuid("user_id").notNull()
+    .references(()=>users.id, {onDelete: 'cascade'}),
+    status: text("status").$type<OrderStatus>().notNull().default("pending"),
+    polarCheckoutId: text("polar_checkout_id"),
+    polarOrderId: text("polar_order_id").unique(),
+   totalCents: integer("total_cents").notNull().default(0),
+    createdAt: timestamp("created_at", {withTimezone:true}).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", {withTimezone:true}).defaultNow().notNull(),
+})
+
+export const orderItems = pgTable("order_items", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    orderId: uuid("order_id").notNull()
+    .references(()=>orders.id, {onDelete:'cascade'}),
+    productId: uuid("product_id").notNull()
+    .references(()=>products.id, {onDelete:'restrict'}),
+    quantity:integer("quantity").notNull(),
+    unitPriceCents: integer("unit_price_cents").notNull(),
+})
+
+//users can have many order lines over time
+export const userRelations = relations(users, ({many})=>({
+    orders:many(orders)
+}))
+
+//the same producs can show up on many order
+export const productRelations = relations(products, ({many})=>({
+    orderItems: many(orderItems)
+}))
+
+//each order belongs to user; each ordeer can have many line items
+export const ordersRelations = relations(orders,({one, many})=>({
+   user: one(users, {fields:[orders.userId], references:[users.id]}),
+   items: many(orderItems) 
+}))
+//each line item if for exactly one order aand one product
+export const orderItemsRelations = relations(orderItems, ({one})=>({
+    order: one(orders,{fields: [orderItems.orderId],references:[orders.id]}),
+    product:one(products, {fields:[orderItems.productId], references:[products.id]})
+}))
