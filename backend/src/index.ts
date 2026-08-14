6cf9cedea1ee843bb21c6fe52173b12e57179e4e
Izaya-Letselane <139796837+Izaya-Letselane@users.noreply.github.com>
2026-08-14 11:56:00 +0200

    3RD COMMIT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,24 @@
 import express from "express"
+import cors from 'cors'
+import {clerkMiddleware} from '@clerk/express'
+import { clerkWebhookHandler } from "./webhooks/clerk.js"
+import { getEnv } from './lib/env.js';
+import "dotenv/config"
 
+
+const env =getEnv()
 const app = express()
+const rawJson = express.raw({type:"application/json", limit:"1mb"})
 
-app.listen(3001, ()=>console.log("listening on port 3001"))
+//it is important that you don't parse the webhook event data. it should be in raw format
+app.post("/webhooks/clerk", rawJson,(req,res)=>{//get rwa json
+    void clerkWebhookHandler(req, res)
+})
+
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+
+
+app.listen(env.PORT, ()=>console.log("listening on port: ", env.PORT))
